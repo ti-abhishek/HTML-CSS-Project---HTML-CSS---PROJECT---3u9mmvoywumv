1164b9605e76ef64d8d18376616f53a57f40d2ea
ti-abhishek <ti.abhishek15@gmail.com>
2022-10-25 20:00:05 +0530

    new

--- App.js
+++ App.js
@@ -1,1 +1,14 @@
+const codes = document.querySelectorAll('.input')
 
+codes[0].focus()
+
+codes.forEach((input, idx) =>{
+    input.addEventListener('keydown' , (e) =>{
+        if(e.key >= 0 && e.key <= 9){
+            codes[idx].value = ''
+            setTimeout(() => codes[idx + 1].focus(), 10)
+        }else if(e.key === 'Backspace') {
+            setTimeout(() => codes[idx - 1].focus(), 10)
+        }
+    })
+})
